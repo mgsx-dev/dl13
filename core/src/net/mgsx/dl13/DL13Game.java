17a8b24919d79bdef39b6e8c0a82410830c54bb1
Germain AUBERT <germain.aubert@gmail.com>
2020-09-25 20:56:02 +0200

    fix

--- core/src/net/mgsx/dl13/DL13Game.java
+++ core/src/net/mgsx/dl13/DL13Game.java
@@ -21,8 +21,8 @@ public class DL13Game extends Game {
 	
 	private static final String PREF_FILE = "dl13";
 	private static final String PREF_KEY = "save";
-	public static final int UIWidth = 1280;
-	public static final int UIHeight = 960;
+	public static final int UIWidth = 1282;
+	public static final int UIHeight = 962;
 	public static final Color skyColor = new Color(.5f, .8f, 1f, 1f);
 	public static final Color neutralColor = new Color(.8f, .8f, .8f, 1f);
 	
